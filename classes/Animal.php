5ee8fd9ac9bd272221c547aa530c2877a89af4fe
NDIONGUE <112211427+MohamedLammineNDIONGUE1601@users.noreply.github.com>
2025-12-17 03:49:27 +0100

    Implement Animal hierarchy with specific behaviors

--- classes/Animal.php
+++ classes/Animal.php
@@ -1,1 +1,45 @@
+<?php
 
+abstract class Animal {
+
+    public function manger() {
+        return "Je mange";
+    }
+
+    public function dormir() {
+        return "Je dors";
+    }
+}
+
+//$animal = new Animal();
+//Cela que la classe ne peut plus d'avoir d'enfant
+final class Chien extends Animal {
+    public function manger() {
+        return " Je mange des croquettes pour chien";
+    }
+}
+
+class Chiot extends Chien {
+
+}
+
+class Chat extends Animal {
+
+    final public function manger() {
+        return " Je mange des croquettes pour chat";
+    }
+
+}
+
+class Chaton extends Chat {
+
+}
+
+$chien = new Chien();
+echo $chien->manger();
+
+$chat = new Chat();
+echo $chat->manger();
+
+$chaton = new Chaton();
+echo $chaton->manger();
